6a960e16ef87a4d36934da9ac30d387e8ac71a41
azul915 <k.aoyagi940516@gmail.com>
2020-11-29 23:38:03 +0900

    update

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+current=$(cd $(dirname $0) && pwd)
+cd $current
+
+echo -e 'Cleaning...'
+rm -rf src/*
+
 echo -e 'docker-compose down -v'
 docker-compose down -v
 
@@ -9,5 +15,9 @@ docker-compose build --no-cache
 echo -e 'docker-compose up -d'
 docker-compose up -d
 
-echo -e 'docker-compose exec jdk /bin/bash'
-docker-compose exec jdk /bin/bash
+echo -e 'gradle init'
+docker-compose exec jdk /bin/sh -c 'cd /usr/local/src && \
+gradle init --dsl=kotlin --package=sandbox --project-name=sandbox --type=kotlin-application'
+
+echo -e 'gradlew run'
+docker-compose exec jdk /bin/sh -c '/usr/local/src/gradlew run'
